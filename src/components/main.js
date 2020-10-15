cbd5850edc6f70c6dd34574079e9a3e7157431cd
msguttman <msguttman@gmail.com>
2020-10-14 22:31:33 -0500

    small cleanup

--- src/components/main.js
+++ src/components/main.js
@@ -9,10 +9,10 @@ class Main extends Component {
             <div className=" flex h-screen w-auto text-gray-600 bg-white">
                 <div className="flex justify-self-center p-24 mx-12">
                     <div className="block p-6">
-                    Hi, I'm Matt Guttman.  I'm unusually curious about the nature of existence.  I've done a lot of thinking about the enduring questions in philosphy.  I'll be writing some of my thoughts here.
-                    <br></br>
-                    <br></br>
-                    I'm an epistemological fallibilist.  I believe knowledge develops through conjectures and criticism.  Part of making progress in any area is being <span className="hover:text-gray-800">often wrong</span>.
+                        Hi, I'm Matt Guttman.  I'm a software engineer and analyst living in Chicago.<br/>
+                        I'm also a philosophy hobbyist.
+                    </div>
+                    <div className="block">
                     </div>
                 </div>
             </div>
