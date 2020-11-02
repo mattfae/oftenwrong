4b8e5001e296568ee17e0e44bd16feb2ebeeaef7
msguttman <msguttman@gmail.com>
2020-11-01 21:32:38 -0600

    small changes

--- src/components/philosophy/philhome.js
+++ src/components/philosophy/philhome.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 function PhilHome() {
 
     return (
-        <p>
-            I'm collecting <span className="underline hover:text-blue-600"><Link to="/philosophy/interconnectedness">pieces</Link></span> of a coherent worldview.
-        </p>
+        <div>
+            <p>I'm collecting <span className="underline hover:text-blue-600"><Link to="/philosophy/interconnectedness">pieces</Link></span> of a coherent worldview.</p>
+        </div>
     );
 }
 
